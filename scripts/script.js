1ed0e444c8535101335c9b0c5a6f45df63a9c137
ArthurChenEvans <arthurchenevans@protonmail.com>
2024-03-11 20:00:01 +0000

    Added script

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,14 @@
 "use strict";
+
+const images = document.querySelectorAll('.image');
+
+images.forEach(image => {
+    image.addEventListener('click', () => {
+        images.forEach(image => {
+            if(image.classList.contains('active')) {
+                image.classList.remove('active');
+            };
+        })
+        image.classList.toggle('active');
+    })
+})
